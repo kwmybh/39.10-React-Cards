ad1a83601a9d28d7123cfb8f743f4b7938d6df4d
Kwame Yeboah <kwmybh@yahoo.com>
2021-09-16 01:24:56 -0400

    custom hooks added

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,32 +1,30 @@
-import React, { useState } from "react";
-import uuid from "uuid";
-import axios from "axios";
-import PlayingCard from "./PlayingCard";
-import "./PlayingCardList.css";
+import React from 'react';
+import { useAxios } from './hooks';
+import PlayingCard from './PlayingCard';
+import { formatCard } from './helpers';
+import './PlayingCardList.css';
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
-  return (
-    <div className="PlayingCardList">
-      <h3>Pick a card, any card!</h3>
-      <div>
-        <button onClick={addCard}>Add a playing card!</button>
-      </div>
-      <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
-        ))}
-      </div>
-    </div>
-  );
+	const [cards, addCard, clearCards] = useAxios(
+		'cards',
+		'https://deckofcardsapi.com/api/deck/new/draw/'
+	);
+	return (
+		<div className="PlayingCardList">
+			<h3>Pick a card, any card!</h3>
+			<div>
+				<button onClick={() => addCard(formatCard)}>Add a playing card!</button>
+				<button onClick={clearCards}>Clear the table</button>
+			</div>
+			<div className="PlayingCardList-card-area">
+				{cards.map((card) => (
+					<PlayingCard key={card.id} front={card.image} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 CardTable.defaultProps = {};
